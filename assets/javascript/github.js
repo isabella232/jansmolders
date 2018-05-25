5db791c1fbae923d353f826888d73204ccf0c9b8
Jan Smolders <jansmolders86@gmail.com>
2018-05-25 15:28:34 +0200

    bump

---
+++ assets/javascript/github.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Instantiate a new GitHub object.
+ *
+ * @param {Object} config An object containing the username, password, type of auth, and the repository to use.
+ *
+ * @constructor
+ */
+
